30e9a567a07f6ba53427cf09b6940210987af7bc
KamranTalpur <kamrantalpur646@gmail.com>
2025-08-20 20:50:33 +0500

    Fixed the issue, (useSelector imported) now it works

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import "./ProductList.css";
 import CartItem from "./CartItem";
 import { addItem } from "./CartSlice";
+import { useSelector } from "react-redux";
 function ProductList({ onHomeClick }) {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
